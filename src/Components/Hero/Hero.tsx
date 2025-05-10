5ad3a0d093037913a296c68f376132a53573f9d0
denizpiyala <159885392+denizpiyala@users.noreply.github.com>
2025-05-10 12:09:10 +0300

    feat: add About, Hero, and Navbar components with styles and assets

--- src/Components/Hero/Hero.tsx
+++ src/Components/Hero/Hero.tsx
@@ -1,11 +1,22 @@
-import './Hero.css'
+import React from 'react';
+import './Hero.css';
+import profile from '../../assets/profile.jpg';
 
-const Hero = () => {
+const Hero: React.FC = () => {
   return (
     <div className='hero'>
-      
+      <img src={profile} alt='profile' className='profile' />
+      <h1><span>hii I'm Deniz Piyala,</span> frontend developer</h1>
+      <p>
+        I'm a frontend developer with a passion for creating beautiful and functional web applications.
+        I love to learn new technologies and improve my skills.
+      </p>
+      <div className="hero-action">
+        <div className="hero-connect">Connect with me</div>
+        <div className="hero-resume">My resume</div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -1,21 +1,20 @@
-import './Navbar.css'
-import logo from '../assets/D (1).png'
+import React from 'react';
+import './Navbar.css';
+import logo from '../../assets/D.png';
 
-const Navbar = () => {
+const Navbar: React.FC = () => {
   return (
     <div className='navbar'>
-        <img src={logo} alt='logo' className='logo' />
-        <ul className='nav-links'>
-            <li>Home</li>
-            <li>About me</li>
-            <li>My Project</li>
-            <li>Contact</li>
-
-            </ul>
-            <div className="nav-connect">Conntect with me</div>
-      
+      <img src={logo} alt='logo' className='logo' />
+      <ul className='nav-links'>
+        <li>Home</li>
+        <li>About me</li>
+        <li>My Project</li>
+        <li>Contact</li>
+      </ul>
+      <div className="nav-connect">Connect with me</div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
